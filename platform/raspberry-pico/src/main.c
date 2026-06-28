bab6377076c1a78373df216ff2cb5469e0ac3f44
Sarijen <sajskyv@gmail.com>
2026-06-28 12:22:00 +0200

    update raspberry example code to reflect current api

--- platform/raspberry-pico/src/main.c
+++ platform/raspberry-pico/src/main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 
@@ -11,14 +13,18 @@
 
 void my_i2c0_init();
 rda_status_t rda5807m_i2c_write(uint8_t i2c_addr, uint8_t* data, uint8_t length);
+rda_status_t rda5807m_i2c_read(uint8_t i2c_addr, uint8_t reg_addr, uint8_t* buff, uint8_t length);
 void global_delay_ms(uint8_t ms);
 
 
 int main() {
+  stdio_init_all();
+
   static rda5807m_t handle;
   handle.i2c_write = rda5807m_i2c_write;
+  handle.i2c_read = rda5807m_i2c_read;
   handle.delay_ms = global_delay_ms;
-
+  
   my_i2c0_init();
   rda5807m_init(&handle);
   rda5807m_tune_frequency(&handle, EXAMPLE_TUNE_FREQUENCY);
@@ -27,23 +33,40 @@ int main() {
 }
 
 
+
+
 void my_i2c0_init() {
   i2c_init(i2c0, I2C0_FREQUENCY_KHZ*1000); 
   gpio_set_function(I2C0_SDA_PIN, GPIO_FUNC_I2C);
   gpio_set_function(I2C0_SCL_PIN, GPIO_FUNC_I2C);
+
+  gpio_pull_up(I2C0_SDA_PIN);
+  gpio_pull_up(I2C0_SCL_PIN);
 }
 
 
 rda_status_t rda5807m_i2c_write(uint8_t i2c_addr, uint8_t* data, uint8_t length) {
   int result = i2c_write_blocking(i2c0, i2c_addr, data, length, false);
 
-  if (result == PICO_OK) {
+  if (result == length) {
     return RDA_OK;
   } else {
     return RDA_I2C_ERROR;
   }
 }
 
+rda_status_t rda5807m_i2c_read(uint8_t i2c_addr, uint8_t reg_addr, uint8_t* buff, uint8_t length) {
+  i2c_write_blocking(i2c0, i2c_addr, &reg_addr, length, true);
+  int result = i2c_read_blocking(i2c0, i2c_addr, buff, length, false);
+
+
+
+  if (result == length) {
+    return RDA_OK;
+  } else {
+    return RDA_I2C_ERROR;
+  }
+}
 
 void global_delay_ms(uint8_t ms) {
   sleep_ms(ms);
