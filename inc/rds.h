cc141e5e45c24c26c9494e095e98d83395198cde
Sarijen <sajskyv@gmail.com>
2026-07-17 23:45:52 +0200

    feat: implement getting datetime from RDS

--- inc/rds.h
+++ inc/rds.h
@@ -1,8 +1,8 @@
 /*
 
-This code follows the official RDS specifications from the RDS Forum.
-Note that this document is protected by a password, I cannot (and it will not be
-uploaded it here for copyright reasons. I recommend getting it yourself, if you are interested in RDS. More information can be found here:
+This code follows the official RDS specifications (IEC 62106) maintained by the RDS Forum.
+Note that this document is protected by a password, I cannot (and will not)
+upload it here for copyright reasons. I recommend getting it yourself, if you are interested in RDS. More information can be found here:
 
 https://www.rdsforum.org/2010/RDS-Specification.htm
 
@@ -14,6 +14,9 @@ https://www.rdsforum.org/2010/RDS-Specification.htm
 #include <stdint.h>
 #include <stdbool.h>
 
+#define GROUP_A 0
+#define GROUP_B 1
+
 typedef struct {
   uint16_t block_a;
   uint16_t block_b;
@@ -24,7 +27,19 @@ typedef struct {
   bool variant;
 } rds_group_t;
 
+
+typedef struct {
+  uint8_t year;
+  uint8_t month;
+  uint8_t day;
+  uint8_t hour;
+  uint8_t minute;
+} rds_datetime_t;
+
+void rds_get_datetime(const rds_group_t* g, rds_datetime_t* dt);
+
 uint8_t rds_get_group_type(const rds_group_t* g);
 bool rds_get_group_variant(const rds_group_t* g);
+
 
 #endif
--- src/rds.c
+++ src/rds.c
@@ -1,8 +1,51 @@
+/*
+
+This code follows the official RDS specifications (IEC 62106) maintained by the RDS Forum.
+Note that this document is protected by a password, I cannot (and will not)
+upload it here for copyright reasons. I recommend getting it yourself, if you are interested in RDS. More information can be found here:
+
+https://www.rdsforum.org/2010/RDS-Specification.htm
+
+*/
+
+
 #include "rds.h"
+
+void rds_get_datetime(const rds_group_t* g, rds_datetime_t* dt) {
+  // Formulas from ETSI EN 300 468 Annex C Figure C.1
+  // Also described in the RDS spec IEC 62106-2 Annex B
+
+
+  // Date
+  float MJD = ((g->block_b & 0x03) << 15) | (g->block_c >> 1);
+
+  uint16_t Yi = (int)((MJD - 15078.2) / 365.25);
+  uint16_t Mi = (int)((MJD - 14956.1 - (int)(Yi * 365.25)) / 30.6001);
+  uint8_t K;
+  if (Mi == 14 || Mi == 15) {K = 1;} else {K = 0;}
+
+  dt->year = (Yi + K) % 100;
+  dt->month = Mi - 1 - K * 12;
+  dt->day = MJD - 14956 - (int)(Yi * 365.25) - (int)(Mi * 30.6001);
+
+
+  // Time
+  uint8_t utc_hour = ((g->block_c & 0x01) << 4) | ((g->block_d >> 12) & 0b1111);
+  uint8_t utc_minute = (g->block_d >> 6) & 0b111111;
+
+  int8_t local_offset = (g->block_d & 0b11111);
+  bool offset_dir = (g->block_d >> 5) & 0x01;
+  local_offset = (offset_dir) ? -local_offset : local_offset;
+
+  dt->hour = (utc_hour + local_offset / 2);
+  dt->minute = (utc_minute + local_offset*30) % 60;
+}
+
 
 uint8_t rds_get_group_type(const rds_group_t* g) {
   return (g->block_b >> 12) & 0x0F;
 }
+
 
 bool rds_get_group_variant(const rds_group_t* g) {
   return (g->block_b >> 11) & 0x01;
